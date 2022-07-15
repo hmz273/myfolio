0b764746a586b89483322cd0120347a089db3851
hmz273 <73111873+hmz273@users.noreply.github.com>
2022-07-15 10:02:43 +0100

    hero & nav section

--- src/compoments/nav/Nav.jsx
+++ src/compoments/nav/Nav.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useState } from 'react'
+import { AiOutlineHome, AiOutlineUser } from 'react-icons/ai'
+import { RiServiceLine } from 'react-icons/ri'
+import { BiBook, BiMessageSquareDetail } from 'react-icons/bi'
 import './Nav.css'
 
 const Nav = () => {
+  const [ activeNav, setActiveNav ] = useState('#')
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
+      <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
+      <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook/></a>
+      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine/></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail/></a>
+    </nav>
   )
 }
 
