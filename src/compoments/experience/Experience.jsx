38d684c6b5d7bf920935546b7d9edf9b9b574eec
hmz273 <73111873+hmz273@users.noreply.github.com>
2022-07-16 13:01:06 +0100

    experience section

--- src/compoments/experience/Experience.jsx
+++ src/compoments/experience/Experience.jsx
@@ -1,9 +1,118 @@
 import React from 'react'
+import { BsPatchCheckFill } from 'react-icons/bs'
 import './Experience.css'
 
 const Experience = () => {
   return (
-    <section id="experience">Experience</section>
+    <section id="experience">
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+          <article className="experience__details">
+            <BsPatchCheckFill className="experience__details-icon"/>
+            <div>
+            <h4>Html</h4>
+            <small className="text-light">Experience</small>
+            </div>
+          </article>
+          
+          <article className="experience__details">
+            <BsPatchCheckFill className="experience__details-icon"/>
+            <div>
+            <h4>Html</h4>
+            <small className="text-light">Experience</small>
+            </div>
+          </article>
+          
+          <article className="experience__details">
+            <BsPatchCheckFill className="experience__details-icon"/>
+            <div>
+            <h4>Html</h4>
+            <small className="text-light">Experience</small>
+            </div>
+          </article>
+          
+          <article className="experience__details">
+            <BsPatchCheckFill className="experience__details-icon"/>
+            <div>
+            <h4>Html</h4>
+            <small className="text-light">Experience</small>
+            </div>
+          </article>
+
+          
+          <article className="experience__details">
+            <BsPatchCheckFill className="experience__details-icon"/>
+            <div>
+            <h4>Html</h4>
+            <small className="text-light">Experience</small>
+            </div>
+          </article>
+
+          
+          <article className="experience__details">
+            <BsPatchCheckFill className="experience__details-icon"/>
+            <div>
+            <h4>Html</h4>
+            <small className="text-light">Experience</small>
+            </div>
+          </article>
+          </div>
+        </div>
+
+{/* ======================= */}
+
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+          <article className="experience__details">
+            <BsPatchCheckFill className="experience__details-icon"/>
+            <div>
+            <h4>nodejs</h4>
+            <small className="text-light">Experience</small>
+            </div>
+          </article>
+          
+          <article className="experience__details">
+            <BsPatchCheckFill className="experience__details-icon"/>
+            <div>
+            <h4>nodejs</h4>
+            <small className="text-light">Experience</small>
+            </div>
+          </article>
+          
+          <article className="experience__details">
+            <BsPatchCheckFill className="experience__details-icon"/>
+            <div>
+            <h4>nodejs</h4>
+            <small className="text-light">Experience</small>
+            </div>
+          </article>
+          
+          <article className="experience__details">
+            <BsPatchCheckFill className="experience__details-icon"/>
+            <div>
+            <h4>nodejs</h4>
+            <small className="text-light">Experience</small>
+            </div>
+          </article>
+
+          
+          <article className="experience__details">
+            <BsPatchCheckFill className="experience__details-icon"/>
+            <div>
+            <h4>nodejs</h4>
+            <small className="text-light">Experience</small>
+            </div>
+          </article>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/compoments/header/Header.jsx
+++ src/compoments/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
       <div className="container header__container">
         <h5>hello i'm</h5>
         <h1>Hamza Elabdi</h1>
-        <h5 className="text-light">Fullstack Developer</h5>
+        <h5 className="text-light">MERN Stack Developer</h5>
         <CTA />
         <HeaderSocials />
         <div className="me">
