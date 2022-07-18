0ecaa4454d668ec4d9b308c7c1526d431fad13a4
hmz273 <73111873+hmz273@users.noreply.github.com>
2022-07-18 14:01:22 +0100

    portfolio section

--- src/compoments/contact/Contact.jsx
+++ src/compoments/contact/Contact.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import emailjs from 'emailjs-com';
 import './Contact.css'
 
 const Contact = () => {
+
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_kbyju9g', 'template_dwx4iz2', form.current, '9W79oyy5--4NValuG')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
+  };
+
+
+
   return (
-    <section id="contact">Contact</section>
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            icon
+            <h4>Email</h4>
+            <h5>hamzaelabdi58@gmail.com</h5>
+            <a href="mailto:hamzaelabdi58@gmail.com">Send a message</a>
+          </article>
+
+          <article className="contact__option">
+            icon
+            <h4>Email</h4>
+            <h5>hamzaelabdi58@gmail.com</h5>
+            <a href="mailto:hamzaelabdi58@gmail.com">Send a message</a>
+          </article>
+
+          <article className="contact__option">
+            icon
+            <h4>Email</h4>
+            <h5>hamzaelabdi58@gmail.com</h5>
+            <a href="mailto:hamzaelabdi58@gmail.com">Send a message</a>
+          </article>
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder="Full-Name" required />
+          <input type="email" name="email" placeholder="Email" required />
+          <textarea name="message" rows="7" required placeholder="Your Message"></textarea>
+          <button type="submit" className="btn btn-primary">Send</button>
+        </form>
+      </div>
+    </section>
   )
 }
 
--- src/compoments/portfolio/Portfolio.jsx
+++ src/compoments/portfolio/Portfolio.jsx
@@ -1,12 +1,64 @@
+/* eslint-disable react/jsx-no-target-blank */
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react'
-import './Portfolio.css'
-import IMG1 from '../../assets/portfolio1.jpg'
-import IMG2 from '../../assets/portfolio2.jpg'
-import IMG3 from '../../assets/portfolio3.jpg'
-import IMG4 from '../../assets/portfolio4.jpg'
-import IMG5 from '../../assets/portfolio5.png'
-import IMG6 from '../../assets/portfolio6.jpg'
+import React from "react";
+import "./Portfolio.css";
+import IMG1 from "../../assets/portfolio1.jpg";
+import IMG2 from "../../assets/portfolio2.jpg";
+import IMG3 from "../../assets/portfolio3.jpg";
+import IMG4 from "../../assets/portfolio4.jpg";
+import IMG5 from "../../assets/portfolio5.png";
+import IMG6 from "../../assets/portfolio6.jpg";
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: "This is a portfolio item title",
+    github: "https://github.com",
+    demo: "https//google.com",
+  },
+
+  {
+    id: 2,
+    image: IMG2,
+    title: "This is a portfolio item title",
+    github: "https://github.com",
+    demo: "https//google.com",
+  },
+
+  {
+    id: 3,
+    image: IMG3,
+    title: "This is a portfolio item title",
+    github: "https://github.com",
+    demo: "https//google.com",
+  },
+
+  {
+    id: 4,
+    image: IMG4,
+    title: "This is a portfolio item title",
+    github: "https://github.com",
+    demo: "https//google.com",
+  },
+
+  {
+    id: 5,
+    image: IMG5,
+    title: "This is a portfolio item title",
+    github: "https://github.com",
+    demo: "https//google.com",
+  },
+
+  {
+    id: 6,
+    image: IMG6,
+    title: "This is a portfolio item title",
+    github: "https://github.com",
+    demo: "https//google.com",
+  },
+];
+
 const Portfolio = () => {
   return (
     <section id="portfolio">
@@ -14,74 +66,27 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-          <a href="#" className="btn" target="_blank">Github</a>
-          <a href="#" className="btn btn-primary" target="_blank">demo</a>
-          </div>
-        </article>
-
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG2} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-          <a href="#" className="btn" target="_blank">Github</a>
-          <a href="#" className="btn btn-primary" target="_blank">demo</a>
-          </div>
-        </article>
-
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG3} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-          <a href="#" className="btn" target="_blank">Github</a>
-          <a href="#" className="btn btn-primary" target="_blank">demo</a>
-          </div>
-        </article>
-
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG4} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-          <a href="#" className="btn" target="_blank">Github</a>
-          <a href="#" className="btn btn-primary" target="_blank">demo</a>
-          </div>
-        </article>
-
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG5} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-          <a href="#" className="btn" target="_blank">Github</a>
-          <a href="#" className="btn btn-primary" target="_blank">demo</a>
-          </div>
-        </article>
-
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG6} alt="" />
-          </div>
-          <h3>This is a portfolio item title</h3>
-          <div className="portfolio__item-cta">
-          <a href="#" className="btn" target="_blank">Github</a>
-          <a href="#" className="btn btn-primary" target="_blank">demo</a>
-          </div>
-        </article>
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className="portfolio__item">
+              <div className="portfolio__item-image">
+                <img src={image} alt="" />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio__item-cta">
+                <a href={github} className="btn" target="_blank">
+                  Github
+                </a>
+                <a href={demo} className="btn btn-primary" target="_blank">
+                  demo
+                </a>
+              </div>
+            </article>
+          );
+        })}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
