6d44048d46ce549a0f24db43e37fdb5cdd5f4910
hmz273 <73111873+hmz273@users.noreply.github.com>
2022-07-19 11:51:13 +0100

    done

--- src/compoments/footer/Footer.jsx
+++ src/compoments/footer/Footer.jsx
@@ -3,7 +3,29 @@ import './Footer.css'
 
 const Footer = () => {
   return (
-    <section id="footer">Footer</section>
+    <footer id="footer">
+      <a href="#" className="footer__logo">Hamza</a>
+
+      <ul className="permalink">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Experience</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Portfolio</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="#">icon</a>
+        <a href="#">icon</a>
+        <a href="#">icon</a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; Hamza Portfolio. All rights reserved.</small>
+      </div>
+
+    </footer>
   )
 }
 
