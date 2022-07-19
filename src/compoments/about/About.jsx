29baab8f933d02094b205a71cd14a822fe347aec
hmz273 <73111873+hmz273@users.noreply.github.com>
2022-07-19 12:21:32 +0100

    done

--- src/compoments/about/About.jsx
+++ src/compoments/about/About.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import "./about.css";
-import ME from "../../assets/me-about.jpg";
+import ME from "../../assets/working.png";
 
 const About = () => {
   return (
