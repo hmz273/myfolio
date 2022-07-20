099cce83edf6d59c2207f68e311b5c3bc3bf7003
hmz273 <73111873+hmz273@users.noreply.github.com>
2022-07-20 12:36:43 +0100

    Fix error

--- src/compoments/footer/Footer.jsx
+++ src/compoments/footer/Footer.jsx
@@ -1,4 +1,6 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
+import { BsLinkedin, BsGithub, BsInstagram } from 'react-icons/bs'
 import './Footer.css'
 
 const Footer = () => {
@@ -8,17 +10,17 @@ const Footer = () => {
 
       <ul className="permalink">
         <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Experience</a></li>
-        <li><a href="#">Services</a></li>
-        <li><a href="#">Portfolio</a></li>
-        <li><a href="#">Contact</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
       </ul>
 
       <div className="footer__socials">
-        <a href="#">icon</a>
-        <a href="#">icon</a>
-        <a href="#">icon</a>
+      <a href="#" target="_blank"><BsLinkedin /></a>
+      <a href="#" target="_blank"><BsInstagram /></a>
+      <a href="#" target="_blank"><BsGithub /></a>
       </div>
 
       <div className="footer__copyright">
--- src/compoments/header/HeaderSocials.jsx
+++ src/compoments/header/HeaderSocials.jsx
@@ -2,13 +2,14 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import { BsLinkedin, BsGithub } from 'react-icons/bs'
+import { FaInstagramSquare } from 'react-icons/fa'
 
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
       <a href="" target="_blank"><BsLinkedin /></a>
       <a href="" target="_blank"><BsGithub /></a>
-      <a href="" target="_blank">icon</a>
+      <a href="" target="_blank"><FaInstagramSquare /></a>
 
     </div>
   )
