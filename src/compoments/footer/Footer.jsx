1392d27eeee6432539ffc09f413a79e52f092c18
hmz273 <73111873+hmz273@users.noreply.github.com>
2023-11-18 12:17:11 +0100

    update CV and contact section

--- src/compoments/footer/Footer.jsx
+++ src/compoments/footer/Footer.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
-import { BsLinkedin, BsGithub, BsInstagram } from 'react-icons/bs'
+import { BsLinkedin, BsGithub } from 'react-icons/bs'
 import './Footer.css'
 
 const Footer = () => {
@@ -12,7 +12,7 @@ const Footer = () => {
         <li><a href="#">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#experience">Experience</a></li>
-        <li><a href="#services">Services</a></li>
+        {/* <li><a href="#services">Services</a></li> */}
         <li><a href="#portfolio">Portfolio</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
--- src/compoments/header/HeaderSocials.jsx
+++ src/compoments/header/HeaderSocials.jsx
@@ -2,14 +2,14 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import { BsLinkedin, BsGithub } from 'react-icons/bs'
-import { FiShoppingCart } from 'react-icons/fi'
+// import { FiShoppingCart } from 'react-icons/fi'
 
 const HeaderSocials = () => {
   return (
     <div className="header__socials">
       <a href="https://www.linkedin.com/in/hamza-elabdi-276828239" target="_blank" rel="noreferrer"><BsLinkedin /></a>
       <a href="https://github.com/hmz273" target="_blank" rel="noreferrer"><BsGithub /></a>
-      <a href="https://www.redbubble.com/people/HhaammZz24/shop" target="_blank" rel="noreferrer"><FiShoppingCart /></a>
+      {/* <a href="https://www.redbubble.com/people/HhaammZz24/shop" target="_blank" rel="noreferrer"><FiShoppingCart /></a> */}
 
     </div>
   )
